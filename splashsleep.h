e9ab3e9d59fca72983808cf72d098d4d471a6ee3
nirnoy-charisma <116044080+nirnoy-charisma@users.noreply.github.com>
2022-12-25 21:06:19 +0600

    Add files via upload

---
+++ splashsleep.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <direct.h>
+#include <dos.h>
+void splash(void)
+
+{
+
+
+
+
+ printf("_______  ______ _______ \n");
+ printf("|_____| |_____/    |        \n");
+ printf("|     | |    \\_   |        \n");
+
+ printf(" ______ _______               _______  ______ __   __ \n");
+ printf("|  ____ |_____| |      |      |______ |_____/   \\_/  \n");
+ printf("|_____| |     | |_____ |_____ |______ |    \\_    |   \n");
+sleep (5);
+system("cls");
+
+sleep(5);
+
+
+    return 0;
+
+
+
+
+}
