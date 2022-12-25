43715b6d844164bd0b75604f54193ae9fa4f205c
nirnoy-charisma <116044080+nirnoy-charisma@users.noreply.github.com>
2022-12-25 22:40:41 +0600

    Add files via upload

--- main.c
+++ main.c
@@ -1,18 +1,48 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include <direct.h>
+#include <dos.h>
+void booking();
+void location();
+void gallery();
+void cancel();
 int main(void)
 
 {
-
-
-
- printf("_______  ______ _______ \n");
- printf("|_____| |_____/    |        \n");
- printf("|     | |    \_    |        \n");
-
- printf(" ______ _______               _______  ______ __   __ \n");
- printf("|  ____ |_____| |      |      |______ |_____/   \\_/  \n");
- printf("|_____| |     | |_____ |_____ |______ |    \\_    |   \n");
+    intoption;
+    printf("Please Select Your Requirements:\n");
+    do {
+            system ("cls");
+        printf("1. Location.\n");
+        printf("\n");
+        printf("2. Gallery Available.\n");
+        printf("\n");
+        printf("3. Booking\n");
+printf("\n");
+        printf("4. Cancel\n");
+        printf("\n");
+        printf("5. Back\n");
+        printf("\n");
+        scanf("%d",&option);
+        switch(option){
+        case 1:
+           location();
+           break;
+        case 2:
+            gallery();
+            break;
+        case 3:
+            booking();
+            break;
+        case 4:
+            cancel();
+            break;
+        }
+        getch();
+    }
+    while (option!=5);
+    printf(" Thanks for trying. \n");
 
 
 
@@ -21,8 +51,24 @@ int main(void)
 
 
     return 0;
+}
+void location ()
+{
+    l[][100]={"Gulshan","Banani","Uttura","Mogbazar","Rampura","Farmgate"};
+    system ("cls");
+   printf("\t\t\t\t\t[1]  =>  %s\n",ch[0]);
+    printf("\n");
+    printf("\t\t\t\t\t[2]  =>  %s\n",ch[1]);
+    printf("\n");
+	printf("\t\t\t\t\t[3]  =>  %s\n",ch[2]);
+    printf("\n");
+	printf("\t\t\t\t\t[4]  =>  %s\n",ch[3]);
+    printf("\n");
+	printf("\t\t\t\t\t[5]  =>  %s\n",ch[4]);
 
-
+}
+void gallery()
+{
 
 
 }
