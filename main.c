89a6f13217877738d88d8fcdd07c1d7324268b84
nirnoy-charisma <116044080+nirnoy-charisma@users.noreply.github.com>
2022-12-30 14:16:07 +0600

    Add files via upload

--- main.c
+++ main.c
@@ -1,134 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <dos.h>
-int main(void)
-
-{ int option, location, booking,gall ,cancel;
-
-printf(" Sorry for the trouble\n");
-printf(" You Need to enter the location and select the Gallery\n");
-printf(" then you will be provided an option the leave.\n");
-printf("Thanks for tagging along\n");
-printf("\n");
-printf("\n");
-
-        //printf("1. Location And Gallery Available.\n");
-        //printf("\n");
-        //printf("2. Booking\n");
-       // printf("\n");
-        //printf("3. Cancel\n");
-        //printf("\n");
+#include <string.h>
+void login_info (void);
+void login (void);
+struct login {
+char user [50];
+char pass [50];
+};
 
 
-printf("\t\t\t\t\t Choose your Location\n");
-scanf("%d",&location);
+void login_info (void)
+{
+ FILE *lol;
+ lol = fopen("up.txt","w");
+ struct login l;
+ printf("enter the username:");
+ scanf("%s", l.user);
+ printf("enter the password:");
+ scanf("%s", l.pass);
+ fwrite (&l,sizeof (l),1,lol);
+ fclose(lol);
 
-switch (location){
-case 1:
-    printf("Gulshan\n");
-    break;
-case 2:
-    printf("Banani\n");
-    break ;
-case 3:
-    printf("Uttura\n");
-    break;
-default:
-    printf("Error!\n");
-    break;
-    }
-
-
-    printf("Choose the available Gallery\n");
- printf("\t\t\t\t\t\t Choose the gallery\n",gall);
- scanf("%d",&gall);
- if (location==1){
-
-    printf("\t\t\t\t\t 1. Edge Gallery\n",gall);
-    printf("\t\t\t\t\t 2.Radius Centre\n",gall);
- printf(" Choose one gallery.\n");
- if (gall=1){
-    printf(" You have choosen Edge Gallery\n");
-
- }
- else
-    printf("You have choosen Radius Gallery\n");
-
-
- }
- else if (location==2){
-    printf("\t\t\t\t\t\ 1.Dhaka Gallery\n",gall);
-    printf("\t\t\t\t\t\ 2.Arcadia Art\n",gall);
-    printf(" Choose one gallery.\n");
-    if (gall=1){
-        printf("You have choosen Dhaka Gallery\n");
-
-    }
-    else
-        printf("You have choosen Arcadia Gallery\n");
- }
- else if (location==3){
-    printf("\t\t\t\t\t\ 1.Cezanne Gallery\n",gall);
-    printf("\t\t\t\t\t\ 2.Yeart Gallery\n",gall);
-    printf(" Choose one gallery.\n");
-    if (gall=1){
-        printf("You have choosen Cezanne Gallery\n");
-
-    }
-    else
-    printf("You have choosen Yeart Gallery\n");
- }
-    else
-        printf("Error!\n");
-
-
-//system ("cls");
-
-       printf(" ***Choose the date*** ");
-       scanf("%d",&booking);
-
-
-        switch (booking)
+}
+ void login (void)
+ {
+     char user[50];
+     char pass [50];
+     FILE *lol;
+     lol = fopen("up.txt","r");
+     struct login l;
+     printf("enter the username and password\n");
+     while (fread(&l, sizeof(l), 1, lol))
+    {
+        while (1)
         {
-            case 1 :
-            printf("***1 Jan 2023 Time:10.00 AM- 10.00 PM***\n");
-            break;
-            case 2:
-                printf("***2 Jan 2023 Time 10.00 Am-10.00 PM***\n");
+            printf("enter the username:");
+            scanf("%s", user);
+            printf("enter the password:");
+            scanf("%s", pass);
+            if (strcmp(user, l.user)==0 && strcmp(pass, l.pass)==0)
+            {
+                printf("welcome \n");
                 break;
-                case 3:
-                    printf("***3 Jan 2023 Time 10.00 Am-10.00 PM***\n");
-                    break;
-                case 4 :
-                    printf("***4 Jan 2023 Time 10.00 Am-10.00 PM***\n");
-                    break;
-                case 5:
-                    printf("***5 Jan 2023 Time 10.00 Am-10.00 PM***\n");
-                    break;
-                default :
-                    printf("*** No Available Dates. Please wait for few days for the update***\n");
-                    break;
-
-
+            }
+            else
+            {
+                printf("fail\n");
+            }
         }
-
-
-        printf(" ***The cost of the gallery :\n");
-        printf(" ***Tk 50000\n");
-        printf(" ***Press 1 to continue.\n");
-        printf(" ***Press 2 to cancel.\n");
-        scanf("%d",&cancel);
-        switch (cancel){
-     case 1:
-        printf(" ***  Your Booking Has Been Confirmed. You will Be charged Tk 50000  ***\n ");
-        break;
-     case 2:
-        printf(" *** Your Booking Wasnt confirmed . Hope to see you soon ***/n");
-        break;
-     default :
-        printf(" Error!\n");
-
-        }
-
-    return 0;
+     }
+     fclose(lol);
+ }
+void loginfinal(void)
+{
+ login_info();
+ login();
+ return 0;
 }
